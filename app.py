9f72687fcdd69bc7a74688f608c9cd1a6a5c538d
Anwin77 <anwindilip7@gmail.com>
2025-03-16 14:13:24 +0000

    revised app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import tensorflow as tf
+import os
 import tensorflow_addons as tfa
 import numpy as np
 from PIL import Image
